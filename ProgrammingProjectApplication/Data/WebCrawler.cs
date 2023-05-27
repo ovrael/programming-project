b2c250ab5987182d8fed5e0f13ac8a813d95294b
ovrael <ovrael@gmail.com>
2023-05-27 22:27:15 +0200

    Updated UI and css

--- ProgrammingProjectApplication/Data/WebCrawler.cs
+++ ProgrammingProjectApplication/Data/WebCrawler.cs
@@ -10,55 +10,51 @@ namespace ProgrammingProjectApplication.Data
 {
     public class WebCrawler
     {
+        public async Task<List<string>> CrawlAsync(string url)
+        {
+            var urlsToCrawl = new List<string> { url };
+            var visitedUrls = new List<string>();
 
-            public async Task<List<string>> CrawlAsync(string url)
+
+
+            var currentUrl = urlsToCrawl.First();
+            urlsToCrawl.Remove(currentUrl);
+            visitedUrls.Add(currentUrl);
+
+            var html = await GetHtmlFromUrlAsync(currentUrl);
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var links = document.DocumentNode.Descendants("a")
+                .Select(a => a.GetAttributeValue("href", null))
+                .Where(href => !String.IsNullOrEmpty(href))
+                .Select(href => MakeAbsoluteUrl(currentUrl, href))
+                .Where(url => !visitedUrls.Contains(url))
+                .ToList();
+
+
+            foreach (var link in links)
             {
-                var urlsToCrawl = new List<string> { url };
-                var visitedUrls = new List<string>();
+                urlsToCrawl.Add(link);
+            }
 
-              
-                
-                    var currentUrl = urlsToCrawl.First();
-                    urlsToCrawl.Remove(currentUrl);
-                    visitedUrls.Add(currentUrl);
-
-                    var html = await GetHtmlFromUrlAsync(currentUrl);
-                    var document = new HtmlDocument();
-                    document.LoadHtml(html);
-
-                    var links = document.DocumentNode.Descendants("a")
-                        .Select(a => a.GetAttributeValue("href", null))
-                        .Where(href => !String.IsNullOrEmpty(href))
-                        .Select(href => MakeAbsoluteUrl(currentUrl, href))
-                        .Where(url => !visitedUrls.Contains(url))
-                        .ToList();
-
-
-                    foreach (var link in links)
-                    {
-                        urlsToCrawl.Add(link);
-                    }
-                
 
             return urlsToCrawl;
-            }
-
-            private async Task<string> GetHtmlFromUrlAsync(string url)
-            {
-                using (var client = new HttpClient())
-                {
-                    return await client.GetStringAsync(url);
-                }
-            }
-
-            private string MakeAbsoluteUrl(string baseUrl, string relativeUrl)
-            {
-                var uri = new Uri(baseUrl);
-                var newUri = new Uri(uri, relativeUrl);
-                return newUri.AbsoluteUri;
-            }
-
         }
 
-    
+        private async Task<string> GetHtmlFromUrlAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.GetStringAsync(url);
+            }
+        }
+
+        private string MakeAbsoluteUrl(string baseUrl, string relativeUrl)
+        {
+            var uri = new Uri(baseUrl);
+            var newUri = new Uri(uri, relativeUrl);
+            return newUri.AbsoluteUri;
+        }
+    }
 }
